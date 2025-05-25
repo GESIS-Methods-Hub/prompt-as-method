e51dec8aeaa1bf556544e4d5956ec1dbe0857cbc
Johannes Kiesel <Johannes.Kiesel@gesis.org>
2025-05-25 12:21:04 +0200

    add __all__

--- src/prompt_as_method/__init__.py
+++ src/prompt_as_method/__init__.py
@@ -1,4 +1,22 @@
+from .llm import LLM, HttpLLM, OpenAI, Ollama
 from .prompt_template import PromptTemplate
+from .prompt import BaseMessage, AssistantMessage, SystemMessage, UserMessage, Message, Messages
+from .prompt import ResponseFormat, PromptParameters, Prompt
 
-__all__ = ["PromptTemplate"]
+__all__ = [
+    "AssistantMessage",
+    "BaseMessage",
+    "HttpLLM",
+    "LLM",
+    "Message",
+    "Messages",
+    "Ollama",
+    "OpenAI",
+    "Prompt",
+    "PromptParameters",
+    "PromptTemplate",
+    "ResponseFormat",
+    "SystemMessage",
+    "UserMessage"
+]
 __version__ = "0.1.0"
